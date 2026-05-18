a04c5219c5bd2f667aacdda0af4d091abcea773f
my <24521092@gm.uit.edu.vn>
2026-05-18 17:11:02 +0700

    create staticstic viewmodel and activity

--- app/src/main/java/com/example/diary_app/ui/pages/statistics/StaticticsActivity.java
+++ app/src/main/java/com/example/diary_app/ui/pages/statistics/StaticticsActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -19,14 +20,20 @@ import com.anychart.charts.Cartesian;
 import com.anychart.charts.Pie;
 import com.anychart.APIlib;
 import com.example.diary_app.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.text.DateFormatSymbols;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class StaticticsActivity extends AppCompatActivity {
+
+    //region khai báo biến
     private StaticticsViewModel viewModel;
     private AnyChartView pieChartView;
     private AnyChartView barChartView;
@@ -38,12 +45,16 @@ public class StaticticsActivity extends AppCompatActivity {
     private ImageButton btnPre;
     private ImageButton btnNext;
     private Calendar calendar;
+    private Date startDate;
+    private Date endDate;
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_statictics);
 
+        //region khởi tạo
         pieChartView = findViewById(R.id.pieChart);
         barChartView = findViewById(R.id.barChart);
         gridCalendar = findViewById(R.id.gridCalendar);
@@ -52,17 +63,43 @@ public class StaticticsActivity extends AppCompatActivity {
         btnDropDown = findViewById(R.id.btnDropDown);
         btnPre = findViewById(R.id.btnPre);
         btnNext = findViewById(R.id.btnNext);
-
+        startDate = new Date();
+        endDate = new Date();
+        //vm
         viewModel = new ViewModelProvider(this).get(StaticticsViewModel.class);
+        //endregion
 
-        viewModel.getMoodData().observe(this, moodData -> {
-            if (moodData != null) showPieChart(moodData);
-        });
-        
-        viewModel.getEmotionData().observe(this, emotionData -> {
-            if (emotionData != null) showBarChart(emotionData);
+        String Uid ="";
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+            Uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        }
+
+        viewModel.getReactionList().observe(this, (List<String> reactions) -> {
+            if(reactions != null && !reactions.isEmpty()){
+                Map<String,Integer> emotionCounts = countReactions(reactions);
+                showPieChart(emotionCounts);
+                showBarChart(emotionCounts);
+            }
+            else{
+                showPieChart(new HashMap<>());
+                showBarChart(new HashMap<>());
+            }
         });
 
+        viewModel.getErrorMessage().observe(this, (String errorMessage) -> {
+            if(errorMessage != null){
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        viewModel.getMoodData().observe(this, moodData -> {
+//            if (moodData != null) showPieChart(moodData);
+//        });
+//
+//        viewModel.getEmotionData().observe(this, emotionData -> {
+//            if (emotionData != null) showBarChart(emotionData);
+//        });
+//
         calendar = Calendar.getInstance();
         calendar.set(Calendar.DAY_OF_MONTH, 1);
         updateCalendar();
@@ -78,13 +115,33 @@ public class StaticticsActivity extends AppCompatActivity {
         });
     }
 
-    private void showPieChart(List<Pair<String, Integer>> moodData){
+    // region show pie chat cũ
+//    private void showPieChart(List<Pair<String, Integer>> moodData){
+//        APIlib.getInstance().setActiveAnyChartView(pieChartView);
+//        Pie pie = AnyChart.pie();
+//        List<DataEntry> datas = new ArrayList<>();
+//        for(Pair<String, Integer> item : moodData){
+//            datas.add(new ValueDataEntry(item.first, item.second));
+//        }
+//        pie.data(datas);
+//        pie.title("Thống kê tâm trạng");
+//        pie.title().fontSize("12");
+//        pie.legend().enabled(false);
+//        pie.palette(new String[]{"#FEFACA", "#CBE5C2", "#D6C7DE", "#CDEBF3", "#F7BDB1"});
+//        pieChartView.setChart(pie);
+//    }
+    //endregion
+
+    private void showPieChart(Map<String, Integer> emotionDatas) {
         APIlib.getInstance().setActiveAnyChartView(pieChartView);
         Pie pie = AnyChart.pie();
-        List<DataEntry> datas = new ArrayList<>();
-        for(Pair<String, Integer> item : moodData){
-            datas.add(new ValueDataEntry(item.first, item.second));
+
+        List<DataEntry>datas = new ArrayList<>();
+
+        for(Map.Entry<String,Integer> item: emotionDatas.entrySet()){
+            datas.add(new ValueDataEntry(item.getKey(), item.getValue()));
         }
+
         pie.data(datas);
         pie.title("Thống kê tâm trạng");
         pie.title().fontSize("12");
@@ -92,14 +149,30 @@ public class StaticticsActivity extends AppCompatActivity {
         pie.palette(new String[]{"#FEFACA", "#CBE5C2", "#D6C7DE", "#CDEBF3", "#F7BDB1"});
         pieChartView.setChart(pie);
     }
+    //region show bar chat cũ
+//    private void showBarChart(List<Pair<String, Integer>> emotionData){
+//        APIlib.getInstance().setActiveAnyChartView(barChartView);
+//        Cartesian bar = AnyChart.cartesian();
+//        List<DataEntry> datas = new ArrayList<>();
+//        for(Pair<String, Integer> item : emotionData){
+//            datas.add(new ValueDataEntry(item.first, item.second));
+//        }
+//        bar.column(datas);
+//        bar.title("Emotion trends");
+//        bar.title().fontSize("12");
+//        bar.palette(new String[]{"#FEFACA", "#CBE5C2", "#D6C7DE", "#CDEBF3", "#F7BDB1"});
+//        barChartView.setChart(bar);
+//    }
+    //endregion
 
-    private void showBarChart(List<Pair<String, Integer>> emotionData){
+    private void showBarChart(Map<String, Integer> emotionDatas){
         APIlib.getInstance().setActiveAnyChartView(barChartView);
         Cartesian bar = AnyChart.cartesian();
         List<DataEntry> datas = new ArrayList<>();
-        for(Pair<String, Integer> item : emotionData){
-            datas.add(new ValueDataEntry(item.first, item.second));
+        for(Map.Entry<String,Integer> item: emotionDatas.entrySet()){
+            datas.add(new ValueDataEntry(item.getKey(), item.getValue()));
         }
+
         bar.column(datas);
         bar.title("Emotion trends");
         bar.title().fontSize("12");
@@ -108,9 +181,9 @@ public class StaticticsActivity extends AppCompatActivity {
     }
 
     private void updateCalendar(){
-        // Luôn đặt về ngày 1 để lấy thứ của ngày đầu tháng
+        //lấy thứ của ngày đầu tháng
         calendar.set(Calendar.DAY_OF_MONTH, 1);
-        
+
         month.setText(new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH)]);
         year.setText(String.valueOf(calendar.get(Calendar.YEAR)));
 
@@ -119,41 +192,11 @@ public class StaticticsActivity extends AppCompatActivity {
 
         gridCalendar.removeAllViews();
 
-
         // 42 ô tất cả
         int totalCells = 42;
         int dayCounter = 1;
+       // 2. Thêm các ngày trong tháng
 
-//        // 1. Thêm các ô trống (offset)
-//        int offset = dayOfWeek - 1; // Số ô trống cần bỏ qua để ngày 1 đúng thứ
-//        for(int i = 0; i < offset; i++){
-//            TextView emptyView = new TextView(this);
-//            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-//            params.width = 0;
-//            params.height = GridLayout.LayoutParams.WRAP_CONTENT;
-//            params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
-//            emptyView.setLayoutParams(params);
-//            gridCalendar.addView(emptyView);
-//        }
-//
-//        // 2. Thêm các ngày trong tháng
-//        for (int day = 1; day <= daysInMonth; day++){
-//            TextView dayView = new TextView(this);
-//            dayView.setText(String.valueOf(day));
-//            dayView.setTextSize(14);
-//            dayView.setTextColor(getResources().getColor(android.R.color.black));
-//            dayView.setGravity(Gravity.CENTER);
-//
-//            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-//            params.width = 0;
-//            params.height = GridLayout.LayoutParams.WRAP_CONTENT;
-//            params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
-//            params.rowSpec = GridLayout.spec(GridLayout.UNDEFINED);
-//            params.setMargins(4, 12, 4, 12);
-//            dayView.setLayoutParams(params);
-//
-//            gridCalendar.addView(dayView);
-//        }
         for (int cell = 1; cell <= totalCells; cell++) {
             TextView dayView = new TextView(this);
             dayView.setGravity(Gravity.CENTER);
@@ -178,5 +221,40 @@ public class StaticticsActivity extends AppCompatActivity {
 
             gridCalendar.addView(dayView);
         }
+        //TÍNH TOÁN THỜI GIAN VÀ KÍCH HOẠT VÀO VIEWMODEL ---
+        Calendar startCal = (Calendar) calendar.clone();
+        startCal.set(Calendar.DAY_OF_MONTH, 1);
+        startCal.set(Calendar.HOUR_OF_DAY, 0);
+        startCal.set(Calendar.MINUTE, 0);
+        startCal.set(Calendar.SECOND, 0);
+        startDate = startCal.getTime();
+
+        Calendar endCal = (Calendar) calendar.clone();
+        endCal.set(Calendar.DAY_OF_MONTH, daysInMonth);
+        endCal.set(Calendar.HOUR_OF_DAY, 23);
+        endCal.set(Calendar.MINUTE, 59);
+        endCal.set(Calendar.SECOND, 59);
+        endDate = endCal.getTime();
+
+        String Uid ="";
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+            Uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        }
+
+        if (!Uid.isEmpty()) {
+            viewModel.getReactionList(Uid, startDate, endDate);
+        }
+    }
+
+    private Map<String, Integer> countReactions(List<String> reactions) {
+        Map<String, Integer> counts = new HashMap<>();
+        for (String r : reactions) {
+            if (counts.containsKey(r)) {
+                counts.put(r, counts.get(r) + 1);
+            } else {
+                counts.put(r, 1);
+            }
+        }
+        return counts;
     }
 }
